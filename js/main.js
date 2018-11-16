b43b66a66ed8adbce175bf2112c0a9a77031dd09
David Stoddard <david@davidstorytime.com>
2018-11-16 07:22:55 -0700

    Began the localStorage
    Added the object and working on the dropdown

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+//Global Variables
+var htReaders = [new Reader()];;
+var htCurrent;
 
+var readerNames = document.getElementById('readerNames');
+var addReader = document.getElementById('addReader');
+var newReader = document.getElementById('newReader');
+
+/*****************OBJECTS*****************/
+//Reader Objects
+function Reader() {
+  this.name = '';
+  this.page = 1;
+  this.AI = 'Alfred';
+}
+
+/*****************FUNCTIONS*****************/
+function loadReaders() {
+  htReaders = JSON.parse(localStorage.htReaders);
+}
+
+function loadCurrent() {
+  htCurrent = JSON.parse(localStorage.htCurrent);
+}
+
+function addNewStyle() {
+  htCurrent = htReaders.length;
+  htReaders[htCurrent] = new Reader();
+
+  htReaders[htCurrent].name = newReader.value;
+  
+  var createOption = document.createElement('option');
+  createOption.id = 'name' + htCurrent;
+  createOption.value = htCurrent;
+
+  var textnode = document.createTextNode(htReaders[htCurrent].name); //Creates text to go in the new element
+  createOption.appendChild(textnode); //Places the element inside an element, as last child
+
+  readerNames.appendChild(createOption); 
+  readerNames.value = htCurrent;
+}
+
+/*****************EVENTS*****************/
+addReader.addEventListener('click', addReaderStyle);
+
+  /*****************ON PAGE LOAD*****************/
+window.onload = function () {
+  if (localStorage.getItem('htReader') != null) {
+    loadReaders();
+  }
+
+  if (localStorage.getItem('htCurrent') != null) {
+    loadCurrent();
+  }
+}
