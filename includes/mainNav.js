6879a0658fb340f936a53d25add88e1cb3f3ba60
David Stoddard <david@davidstorytime.com>
2018-11-27 12:38:18 -0700

    Create Header Include
    Creating a new include for the pages.

---
+++ includes/mainNav.js
@@ -0,0 +1,4 @@
+document.write("<header>");
+document.write("  <a href='../index.html' class='homeLink'><img id='smallLogo' src='../assets/basic/HeadsOrTales.svg'>");
+document.write("  <span><span class='primaryColor'>HEADS</span> or <span class='shadowColor'>TALES</span><span></a>");
+document.write("</header>");
